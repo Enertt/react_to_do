db9014358e7783285545b32906ba195af243cd1f
y_zavorotynskyi <97520658+Enertt@users.noreply.github.com>
2023-08-21 14:36:09 +0200

    Delete src directory

--- src/Components/Nav/NavContainer.js
+++
@@ -1,8 +0,0 @@
-import Nav from "./Nav";
-import { connect } from 'react-redux';
-
-let mapStateToProps = (state) => ({
-    theme: state.userDataReduser.theme,
-});
-
-export default connect(mapStateToProps, {}) (Nav);
--- src/Components/Settings/SettingsContainer.js
+++
@@ -1,8 +0,0 @@
-import Settings from './Settings'
-import { connect } from 'react-redux';
-
-let mapStateToProps = (state) => ({
-    theme: state.userDataReduser.theme,
-});
-
-export default connect(mapStateToProps, {}) (Settings);
